11cd8a8613fa98eeba5c8b79783901957b410dde
Negan <negan@Negan.local>
2023-07-19 17:29:44 +0700

    Extracting the Reminder Row Into a Separate View

--- MakeItSo/Features/Reminders/ModelViews/RemindersListViewModel.swift
+++ MakeItSo/Features/Reminders/ModelViews/RemindersListViewModel.swift
@@ -10,14 +10,14 @@ import Foundation
 class RemindersListViewModel: ObservableObject {
     @Published
     var reminders = Reminder.samples
-    
+
     func addReminder(_ reminder: Reminder) {
-      reminders.append(reminder)
+        reminders.append(reminder)
     }
-    
+
     func toggleCompleted(_ reminder: Reminder) {
-      if let index = reminders.firstIndex(where: { $0.id == reminder.id} ) {
-        reminders[index].isCompleted.toggle()
-      }
+        if let index = reminders.firstIndex(where: { $0.id == reminder.id }) {
+            reminders[index].isCompleted.toggle()
+        }
     }
 }
